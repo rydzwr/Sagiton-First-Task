7150ce3d99b5d8308ba9568c52fdaa63e7c65b56
rydzwr <rydzwr@gmail.com>
2022-08-24 04:13:14 +0200

    Removed necessary variable

--- src/main/java/FirstTask.java
+++ src/main/java/FirstTask.java
@@ -44,31 +44,8 @@ public class FirstTask
 
     public boolean sanitizeFilename(StringBuilder in)
     {
-
-        String message = in.toString();
-        /*
-        char[] charArray = message.toCharArray();
-        boolean foundSpace = true;
-
-        for (int i = 0; i < charArray.length; i++)
-        {
-            if (Character.isLetter(charArray[i]))
-            {
-                if (foundSpace)
-                {
-                    charArray[i] = Character.toUpperCase(charArray[i]);
-                    foundSpace = false;
-                }
-            }
-            else
-                foundSpace = true;
-        }
-        message = String.valueOf(charArray);
-
-
-         */
-        message = message.replaceAll(" ", "");
-        String userInput = message;
+        String userInput = in.toString();
+        userInput = userInput.replaceAll(" ", "");
 
         for (String validInput : validInputs)
         {
